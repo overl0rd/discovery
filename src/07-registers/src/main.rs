be2f6bdb9ea19b570c30a77712bb8cf30bd1f9ce
overl0rd <overl0rdid@gmail.com>
2021-07-16 00:06:17 +0400

    Safe API using example

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,31 +1,26 @@
 #![no_main]
 #![no_std]
 
-use core::ptr;
-
+use aux7::stm32::gpioa::brr::BR11_AW;
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux7::{entry, iprintln, RegisterBlock, ITM};
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let gpioe = aux7::init().1;
 
-    unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+    // Turn on the North LED
+    gpioe.bsrr.write(|w| w.bs9().set_bit());
 
-        // Turn on the "North" LED (red)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32,1 << 9);
+    // Turn on the East LED
+    gpioe.bsrr.write(|w| w.bs11().set_bit());
 
-        // Turn on the "East" LED (green)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32,1 << 11);
+    // Turn off the North LED
+    gpioe.bsrr.write(|w| w.br9().set_bit());
 
-        // Turn off the "North" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32,1 << (9 + 16));
+    // Turn off the East LED
+    gpioe.bsrr.write(|w| w.br11().set_bit());
 
-        // Turn off the "East" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32,1 << (11 + 16));
-    }
-
+    gpioe.bsrr.write(|w| w.br12().set_bit());
     loop {}
 }
