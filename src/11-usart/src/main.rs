173c29e863456df8fbf12a0e6b833de1b3901156
overl0rd <overl0rdid@gmail.com>
2021-08-09 22:59:14 +0400

    Send a whole string from MC to computer - iter 1

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -9,9 +9,12 @@ fn main() -> ! {
     let (usart1, mono_timer, itm) = aux11::init();
 
     // Send a single character
-    usart1
-        .tdr
-        .write(|w| unsafe { w.tdr().bits(u16::from(b'X')) });
+
+    for c in "The quick brown fox jumps over the lazy dog.".chars() {
+        usart1
+            .tdr
+            .write(|w| unsafe { w.tdr().bits(u16::from(c as u16)) });
+    }
 
     loop {}
 }
