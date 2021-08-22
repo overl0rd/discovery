8e63f49c2c6b35809fef9e681a8f90c50677dbd9
overl0rd <overl0rdid@gmail.com>
2021-08-22 22:54:07 +0400

    Implement the uprint!

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -1,29 +1,52 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => {
+        $serial.write_fmt(format_args!($($arg)*)).ok()
+    };
+}
+
+macro_rules! uprintln {
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\n"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for char in s.chars() {
+            // wait until it's safe to write to TDR
+            while self.usart1.isr.read().txe().bit_is_clear() {} // <- NEW!
+
+            self.usart1
+                .tdr
+                .write(|w| w.tdr().bits(u16::from(char as u16)));
+        }
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let (usart1, mono_timer, mut itm) = aux11::init();
+    let (usart1, _mono_timer, _itm) = aux11::init();
 
-    let instant = mono_timer.now();
-    // Send a string
-    for char in "The quick brown fox jumps over the lazy dog.".chars() {
-        // wait until it's safe to write to TDR
-        while usart1.isr.read().txe().bit_is_clear() {} // <- NEW!
+    let mut serial = SerialPort { usart1 };
 
-        usart1.tdr.write(|w| w.tdr().bits(u16::from(char as u16)));
-    }
-    let elapsed = instant.elapsed(); // in ticks
-
-    iprintln!(
-        &mut itm.stim[0],
-        "`for` loop took {} ticks ({} us)",
-        elapsed,
-        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
-    );
+    uprintln!(serial, "The answer is {}", 40 + 2);
 
     loop {}
 }
